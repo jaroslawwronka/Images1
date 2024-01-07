13b01a108aae3318952a11925a089f73729789b8
Jarek <jaroslaw.wronka@luxmed.pl>
2024-01-07 11:00:57 +0100

    Initial Commit

---
+++ Images1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Images1
+//
+//  Created by Boss on 07/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TrashImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
